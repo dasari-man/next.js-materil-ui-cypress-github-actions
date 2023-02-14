95f3d6652d20bd1698ffaedd4e0fc28cad2b1361
dasari <srikanth.dasari@outlook.com>
2023-02-14 00:41:23 -0500

    cypress cloud configs

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  experimentalWebKitSupport: true,
+  projectId: 'ys87qm',
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
